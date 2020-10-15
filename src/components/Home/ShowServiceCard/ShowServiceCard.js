cddd8a103ccc894dba2c9b644663cef049aaeb71
Kazi Mashry <kazimashry@gamil.com>
2020-10-16 01:29:30 +0600

    service card animation added

--- src/components/Home/ShowServiceCard/ShowServiceCard.js
+++ src/components/Home/ShowServiceCard/ShowServiceCard.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 const ShowServiceCard = (props) => {
     const { name, description, _id } = props.service;
     return (
-        <div className="card mr-4 shadow mt-4 col-md-3" style={{borderRadius:"20px"}}>
+        <div className="card mr-4 shadow mt-4 col-md-3 serviceCard" style={{ borderRadius: "20px" }}>
             <Link to={`/customer/order/${_id}`} style={{ textDecoration: 'none', color: 'black' }}>
-            <div className="d-flex justify-content-center mt-4">
-                <img className="card-img-top img-fluid" style={{width:"20%"}} src={`data:image/png;base64,${props.service.image.img}`} alt="" />
-            </div>
+                <div className="d-flex justify-content-center mt-4">
+                    <img className="card-img-top img-fluid serviceCard-image" style={{ width: "20%" }} src={`data:image/png;base64,${props.service.image.img}`} alt="" />
+                </div>
 
-            <div className="card-body text-center">
-                <h5 className="card-title font-weight-bolder">{name}</h5>
-                <div className="card-title">{description}</div>
-            </div>
-        </Link>
+                <div className="card-body text-center">
+                    <h5 className="card-title font-weight-bolder name">{name}</h5>
+                    <div className="card-title description">{description}</div>
+                </div>
+            </Link>
         </div>
     );
 };
